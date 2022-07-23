dfd970c9c270139761640c342c9fe6932f025832
wuzhenghong <2015129064@qq.com>
2022-07-23 15:29:56 +0800

    test: start game

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,9 @@
 import { describe, expect, it } from 'vitest'
+import { start } from '../src/kiana'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+describe('start game', () => {
+  it('expect a idiom start with 金', () => {
+    const game = start('一字千金').play()
+    expect(game.data.value).toMatch(/^金/)
   })
 })
